90e91d79f3b862eb306545e6bbc3487b17282a8d
Hank W Ibell <hwibell@us.ibm.com>
2023-08-11 15:47:20 -0400

    Add a new test class

---
+++ src/test/java/it/io/openliberty/sample/AnotherBlankTest.java
@@ -0,0 +1,15 @@
+package it.io.openliberty.sample;
+
+import org.junit.jupiter.api.Test;
+
+public class AnotherBlankTest {
+    @Test
+    public void EmptyTest() {
+        System.out.println("Empty test ran!");
+    }
+
+    @Test
+    public void AnotherEmptyTest() {
+        System.out.println("Another empty test ran!");
+    }
+}
